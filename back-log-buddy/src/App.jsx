3dfa8b25b87e2cb03c382a43003402ddd01e3efe
Daniel Byrne <dbyrne978@gmail.com>
2023-07-31 22:30:11 -0600

    Add mock data to display when testing.

--- back-log-buddy/src/App.jsx
+++ back-log-buddy/src/App.jsx
@@ -2,12 +2,56 @@ import './App.css'
 import MediaCard from './MediaCard.jsx'
 
 function App() {
+  const mockGame1 = {
+    mediaName : "Final Fantasy XII",
+    mediaType : "Game",
+    mediaInfo : "GOAT Final Fantasy. Truly the best world and gameplay.",
+    mediaImage : null,
+  }
+  const mockGame2 = {
+    mediaName : "Pikmin 4",
+    mediaType : "Game",
+    mediaInfo : "Highly anticipated. Just look at those little guys!",
+    mediaImage : null,
+  }
+  const mockBook1 = {
+    mediaName : "A Game of Thrones",
+    mediaType : "Book",
+    mediaInfo : "Amid plots and counter-plots, tragedy and betrayal, victory and terror, allies \
+      and enemies, the fate of the Starks hangs perilously in the balance, as each side endeavors \
+      to win that deadliest of conflicts: the game of thrones.",
+    mediaImage : null,
+  }
+  const mockShow1 = {
+    mediaName : "Loki",
+    mediaType : "TV Show",
+    mediaInfo : "The mercurial villain Loki resumes his role as the God of Mischief in a new \
+      series that takes place after the events of “Avengers: Endgame.”",
+    mediaImage : null,
+  }
+  const mockMovie1 = {
+    mediaName : "Dungeons & Dragons: Honor Among Thieves",
+    mediaType : "Movie",
+    mediaInfo : "A charming thief and a band of unlikely adventurers undertake an epic heist to \
+      retrieve a lost relic, but things go dangerously awry when they run afoul of the wrong \
+      people.",
+    mediaImage : null,
+  }
+  const mockMedia = [mockGame1, mockGame2, mockBook1, mockShow1, mockMovie1]
 
   return (
     <>
-      <MediaCard />
-      <MediaCard />
-      <MediaCard />
+      {mockMedia.map((media) => {
+        return (
+          <MediaCard
+            key={media.mediaName}
+            mediaName={media.mediaName}
+            mediaType={media.mediaType}
+            mediaInfo={media.mediaInfo}
+            mediaImage={media.mediaImage}
+            />
+        );
+      })}
     </>
   )
 
--- back-log-buddy/src/MediaCard.jsx
+++ back-log-buddy/src/MediaCard.jsx
@@ -1,10 +1,16 @@
-function MediaCard() {
-  return <div style={{ backgroundColor: 'green' }}>
-    <h3>MediaName</h3>
-    <p>MediaType</p>
-    <p>MediaInfo</p>
-    <img id="mediaImage" src="/placeholder-media-image.bmp" alt="placeholder media image" />
-  </div>;
+function MediaCard({ mediaName, mediaType, mediaInfo, mediaImage }) {
+  return (
+  <div style={{ backgroundColor: 'green' }}>
+    <h3>{mediaName}</h3>
+    <p>{mediaType}</p>
+    <p>{mediaInfo}</p>
+    <img
+      id="mediaImage"
+      src={mediaImage ? mediaImage : "/placeholder-media-image.bmp"}
+      alt="placeholder media image"
+      />
+  </div>
+  );
 }
 
 export default MediaCard
